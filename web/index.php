ab2a18e69b6ba3b3dad4d9bcdf46b0f112249e82
Loïc macbook <loic@emagma.fr>
2015-02-25 23:34:16 +0100

    Added initializr skeleton and twig provider

--- web/index.php
+++ web/index.php
@@ -3,9 +3,19 @@
 require_once __DIR__.'/../vendor/autoload.php'; 
 
 $app = new Silex\Application(); 
+$app['debug'] = true;
 
+// Register twig
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../src/Views',
+));
+
+
+// Main controller
 $app->get('/hello/{name}', function($name) use($app) { 
-    return 'Hello '.$app->escape($name); 
+    return $app['twig']->render('layout.html.twig', array(
+        'name' => $name,
+    ));
 }); 
 
 $app->run(); 
