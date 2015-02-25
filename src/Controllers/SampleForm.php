0a34cfb8e8fa9fb31a989a3435f012b2f1d2a68a
Loïc <loic@emagma.fr>
2015-02-25 19:21:20 +0100

    Playing with form controller

---
+++ src/Controllers/SampleForm.php
@@ -0,0 +1,36 @@
+<?php
+use Symfony\Component\HttpFoundation\Request;
+$controller = $app['controllers_factory'];
+
+$controller->get('/', function (Request $request) use ($app) {
+    // some default data for when the form is displayed the first time
+    $data = array(
+        'name' => 'Your name',
+        'email' => 'Your email',
+    );
+
+    $form = $app['form.factory']->createBuilder('form', $data)
+        ->add('name')
+        ->add('email')
+        ->add('gender', 'choice', array(
+            'choices' => array(1 => 'male', 2 => 'female'),
+            'expanded' => true,
+        ))
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+
+        // do something with the data
+
+        // redirect somewhere
+        return $app->redirect('...');
+    }
+
+    // display the form
+    return $app['twig']->render('index.twig', array('form' => $form->createView()));
+});
+
+return $controller;
--- web/index.php
+++ web/index.php
@@ -1,11 +1,18 @@
 <?php
+require_once __DIR__.'/../vendor/autoload.php';
 
-require_once __DIR__.'/../vendor/autoload.php'; 
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Provider\FormServiceProvider;
 
 $app = new Silex\Application(); 
+$app['debug'] = true;
+
+$app->register(new FormServiceProvider());
 
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
 }); 
+
+$app->mount('/form', include(__DIR__ . '/../src/Controllers/SampleForm.php'));
 
 $app->run(); 
