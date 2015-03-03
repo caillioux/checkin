3088a2db2afc508ff1dd43218250ae8b01f42aa9
Loïc macbook <loic@emagma.fr>
2015-03-03 16:38:42 +0100

    Added few sample controllers

--- web/index.php
+++ web/index.php
@@ -2,18 +2,33 @@
 
 require_once __DIR__.'/../vendor/autoload.php'; 
 
-$app = new Silex\Application(); 
+$app = new Silex\Application();
 $app['debug'] = true;
 
-// Register twig
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/../src/Views',
-));
+// Homepage controller
+$app->get('/', function() use($app) { 
+    return 'Ma page d\'accueil';
+}); 
 
-
-// Main controller
+// Hello controller
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello ' . $name;
 }); 
+
+// Sample controller
+$app->get('/mon-blog-post', function() use($app) { 
+    return 'Ici apparaitra le contenu du post';
+}); 
+
+// Admin event controller
+$app->get('/admin/event/list', function() use($app) { 
+    return 'Ici apparaitra la liste des événements';
+}); 
+
+// Admin contact controller
+$app->get('/admin/contact/list', function() use($app) { 
+    return 'Ici apparaitra la liste des contacts';
+}); 
+
 
 $app->run(); 
